b9330dda456c1d9faa9cae76d78db3a5ea71ef47
Ian <ian.edwards@uconn.edu>
2023-05-03 02:07:39 -0400

    Modified App.jsx

--- react_bank/src/App.jsx
+++ react_bank/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { Link, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Profile from "./pages/Profile"
@@ -8,12 +8,24 @@ import './App.css'
 function App() {
 
   return (
-
+    <>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login </Link>
+        </li>
+      </ul>
+    </nav>
     <Routes>
       <Route path ="/" element={<Home />}/>
       <Route path ="/login" element={<Login />}/>
       <Route path ="/profile" element={<Profile />}/>
     </Routes>
+    </>
+   
 
   )
 }
